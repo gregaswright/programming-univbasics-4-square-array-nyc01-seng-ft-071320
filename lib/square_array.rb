490e08867f5c289eebbd1362df3d6f3979680b7f
Gregory A.S. Wright <geslim@gmail.com>
2020-06-25 21:03:09 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,11 @@
 def square_array(array)
   new_numbers = []
   index = 0
-  while new_numbers < array.length do
-    new_numbers.push(array[index] ** index)
+  numbers = 1
+  while index < array.length do
+    new_numbers.push(array[index] ** numbers)
     index += 1
+    numbers += 1
   end
   new_numbers
 end
